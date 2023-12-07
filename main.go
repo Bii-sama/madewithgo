e578f937d699732f5432174cb89879335020a6c0
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-07 11:32:28 +0100

    go again

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("'Hello, World")
+	fmt.Println("Hello, World")
+
+	var numOne string = "two"
+
+	fmt.Println(numOne)
+
+	var numTwo = "three"
+
+	numsix := "seven" //this method cannot be used outside a function
+
+	fmt.Println(numOne, numTwo, numsix)
 }
