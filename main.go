9cd94cb1461a0d17c531dedcc1a5573627675505
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-07 12:14:36 +0100

    go floats and ints

--- main.go
+++ main.go
@@ -3,15 +3,39 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello, World")
+	// fmt.Println("Hello, World")
 
-	var numOne string = "two"
+	// var numOne string = "two"
 
-	fmt.Println(numOne)
+	// fmt.Println(numOne)
 
-	var numTwo = "three"
+	// var numTwo = "three"
 
-	numsix := "seven" //this method cannot be used outside a function
+	// numsix := "seven" //this method cannot be used outside a function
 
-	fmt.Println(numOne, numTwo, numsix)
+	// fmt.Println(numOne, numTwo, numsix)
+
+	var one int = 20
+	var two = 60
+	three := 100
+
+	fmt.Println(one, two, three)
+
+	//bits and memory
+
+	var aNumber int8 = 25 //int8        the set of all signed  8-bit integers (-128 to 127)
+
+	var anotherNumber int16 = 129  // int16       the set of all signed 16-bit integers (-32768 to 32767)
+
+	var thirdNumber uint = 253 //uint works for only positive numbers
+
+	//floats- Used for decimals
+
+	var floatOne float32 = 98.76
+	floatTwo := 35.88
+
+
+	fmt.Println(floatOne, floatTwo, aNumber, anotherNumber, thirdNumber)
+
+
 }
