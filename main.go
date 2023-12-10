b6e7cbcc65c8fd616f006a0fe1f08c9b95b700c8
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-11 00:36:26 +0100

    User input from terminal

--- main.go
+++ main.go
@@ -1,21 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err:= r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill()bill  {
+	reader := bufio.NewReader(os.Stdin) 
+
+	name, _:= getInput("Create New Bill name :", reader)
+
+	b := newBill(name)
+
+	fmt.Println("Created the new bill -", b.name)
+
+	return b
+}
+
+func promptOptions(b bill)  {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _:= getInput("Choose option(a - add item, s - save a bill, t - add tip):", reader)
+
+	switch opt{
+	case "a":
+		fmt.Println("I chose a")
+	case "s":
+		fmt.Println("I chose s")
+	case "t":
+		fmt.Println("I chose t")
+	}
+
+	// fmt.Println(opt)
+}
+
+//This is how to get and read data that is being passed in via the user input. It is used to read the 
+//information the user types in. os.Stdin is used when the data is being passed from the terminal
 
 func main()  {
 
-	myBill := newBill("Bidemi's Bill")
+	myBill := createBill()
+	promptOptions(myBill)
 
-	myBill.addItem("chicken", 3.50)
-	myBill.addItem("bacon", 7.50)
-	myBill.addItem("coffee", 1.50)
-	myBill.addItem("baked beans", 2.50)
-
-	myBill.addTips(9)
-
-	fmt.Println(myBill.format())
+	fmt.Println(myBill)
 	
 }
 
