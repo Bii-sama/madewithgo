4d72e861bb2fb3f874abe281f6d6966c2cca422a
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-10 19:37:43 +0100

    pointers

--- main.go
+++ main.go
@@ -6,9 +6,46 @@ import (
 	"fmt"
 )
 
+
+
 // var score = 88.7
 
+func updateName(x *string){
+     *x = "Sasha"
+
+	//  return x
+}
+
+
+
+
+// func updateMenu(y map[string]float64){
+// 	y["bread"] = 2.99
+// }
+
+
 func main()  {
+
+name := "Anikah"
+
+
+
+// fmt.Println("memory block:",&name) //&var shows the memory location of the name variable.  memory block: 0xc000014070
+
+//We can also store the memory block of a variable in another variable. Example:
+
+m := &name
+fmt.Println(name)
+updateName(m)
+// fmt.Println(m)
+// fmt.Println("value at memory address is:", *m)// so in order to get the original value , just add *.
+fmt.Println(name)
+
+//Once you see an asterisk in a fuction
+
+
+
+
 
 // 	sayHello("Hina-chan")
 
@@ -22,22 +59,30 @@ func main()  {
 		"salad": 3.12,
 	}
 
+	// updateMenu(menu)
 	fmt.Println(menu)
-	fmt.Println(menu["salad"])
+	// fmt.Println(menu["salad"])
 
-	//looping through maps
+	// //looping through maps
 
-	for k, v  := range menu{
-		fmt.Println(k, "-", v)
-	}
+	// for k, v  := range menu{
+	// 	fmt.Println(k, "-", v)
+	// }
 
-	//ints as keys
+	// //ints as keys
 
-	phoneNo := map[int]string{
-		1234567890: "Armin",
-		2345678901: "Mikasa",
-		3456789012: "Eren",
-	}
+	// phoneNo := map[int]string{
+	// 	1234567890: "Armin",
+	// 	2345678901: "Mikasa",
+	// 	3456789012: "Eren",
+	// }
 
+	// fmt.Println(phoneNo)
+	// fmt.Println(phoneNo[2345678901])
+
+
+	//Pass by values
+
+	
 
 }
